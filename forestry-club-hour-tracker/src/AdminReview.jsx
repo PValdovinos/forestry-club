eb53eeea2c0d70b02ff32e5601389b77ec2e132f
RJ Trenchard <rjtrenchard@gmail.com>
2025-02-14 18:45:27 -0800

    add routing table

---
+++ forestry-club-hour-tracker/src/AdminReview.jsx
@@ -0,0 +1,15 @@
+const test_data = {
+    ['date']: Date.now(),
+    ['name']: 'John Doe',
+    ['hours']: 7.5
+}
+
+function AdminReview() { 
+    return (
+        <>
+            <p>AdminReview Success</p>
+        </>
+    );
+}
+
+export default AdminReview;
