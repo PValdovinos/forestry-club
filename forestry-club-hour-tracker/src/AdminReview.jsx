7b6ae19d28b336f206cdece07d025829293a939d
RJ Trenchard <rjtrenchard@gmail.com>
2025-02-24 19:44:48 -0800

    add date objects to admin review

--- forestry-club-hour-tracker/src/AdminReview.jsx
+++ forestry-club-hour-tracker/src/AdminReview.jsx
@@ -6,12 +6,16 @@ const test_data = [
     {
         ['name']: 'John Doe',
         ['time_in']: Date.now(),
-        ['time_out']: Date.now()
+        ['time_out']: Date.now(),
+        ['date_volunteered']: Date.now(),
+        ['date_submitted']: Date.now()
     },
     {
-        ['name']: 'John Doe',
+        ['name']: 'Bob Johnson',
         ['time_in']: Date.now() + 1,
-        ['time_out']: Date.now() + 1
+        ['time_out']: Date.now() + 1,
+        ['date_volunteered']: Date.now(),
+        ['date_submitted']: Date.now() + 1
     }
 ]
 
@@ -22,10 +26,12 @@ function AdminReview() {
                 {
                     test_data.map(o =>
                         <AdminNotify
-                            key={o.name + o.time_in.toString()}
+                            key={o.name + o.date_submitted.toString()}
                             name={o.name}
                             time_in={o.time_in}
                             time_out={o.time_out}
+                            date_submitted={o.date_submitted}
+                            date_volunteered={o.date_volunteered}
                         />
                     )
                 }
