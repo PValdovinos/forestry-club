fe4b2bd7c5f81ecb65055e5caf01e45516a2ee06
EBoyd3443 <boyd.eric@student.greenriver.edu>
2025-03-07 20:41:07 -0800

    Add Dropdown select component. Call Dropdown select component on home page.

--- forestry-club-hour-tracker/src/pages/Home.jsx
+++ forestry-club-hour-tracker/src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import Dropdown from "../components/Dropdown.jsx";
+
 function Home() { 
+
+    const exampleData = [
+        `Liam Bennett`,
+        `Sophia Carter`,
+        `Ethan Mitchell`,
+        `Isabella Flores`,
+        `Noah Patterson`,
+        `Ava Richardson`,
+        `Mason Brooks`,
+        `Emily Griffin`,
+        `Lucas Hayes`,
+        `Chloe Sullivan`,
+    ];
+
     return (
         <>
             <p>Home</p>
+            <Dropdown id="name-select" data={exampleData} />
             <div><a href='/adminReview'>Admin View</a></div>
             <div><a href='/member'>Member View</a></div>
             <div><a href='/adminClub'>Admin Club View</a></div>
