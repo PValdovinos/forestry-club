de405da6243328701b6158cff5436d308f57585e
EBoydGRC <EBoydGRC@noreply.github.com>
2025-05-12 19:27:18 -0700

    Fix AdminReview and AdminNotify to work with new api. Remove columns and rows from App.jsx.

--- forestry-club-hour-tracker/src/components/AdminNotify.jsx
+++ forestry-club-hour-tracker/src/components/AdminNotify.jsx
@@ -4,30 +4,32 @@ import PropTypes from 'prop-types';
 import { Row, Col } from 'react-bootstrap';
 import { useState } from 'react';
 
-function AdminNotify({ id, name, time_in, time_out, date_volunteered, date_submitted, onAccept = ()=>{}, onDeny = ()=>{} }) {
+function AdminNotify({ id, name, time_in, time_out, date_volunteered, date_submitted, onAccept = ()=>{}, onDeny = ()=>{}, setReviewData = ()=>{}, reviewData }) {
 
     const [show, setShow] = useState(true);
 
     const handleHide = () => setShow(false);
 
     const submission_id = id;
-    const time_in_readable = new Date(time_in).toLocaleTimeString();
-    const time_out_readable = new Date(time_out).toLocaleTimeString();
-    const date_volunteered_readable = new Date(date_volunteered).toLocaleDateString();
+    const time_in_readable = time_in;
+    const time_out_readable = time_out;
+    const date_volunteered_readable = date_volunteered;
     const date_submitted_readable = new Date(date_submitted).toLocaleDateString();
 
     const accept = (id) => {
+        setReviewData(reviewData.filter(entry => entry.submission_id != id));
         handleHide();
         onAccept(id);
     }
 
     const deny = (id) => {
+        setReviewData(reviewData.filter(entry => entry.submission_id != id));
         handleHide();
         onDeny(id);
     }
 
     return (
-        <Toast show={show}>
+        <Toast show={show} style={{margin: "1%",}}>
             <Toast.Header closeButton={false}>
                 <strong>{name}</strong>
             </Toast.Header>
@@ -57,12 +59,14 @@ function AdminNotify({ id, name, time_in, time_out, date_volunteered, date_submi
 AdminNotify.propTypes = {
     id: PropTypes.number,
     name: PropTypes.string,
-    time_in: PropTypes.instanceOf(Date),
-    time_out: PropTypes.instanceOf(Date),
-    date_volunteered: PropTypes.instanceOf(Date),
+    time_in: PropTypes.string,
+    time_out: PropTypes.string,
+    date_volunteered: PropTypes.string,
     date_submitted: PropTypes.instanceOf(Date),
     onAccept: PropTypes.func,
-    onDeny: PropTypes.func
+    onDeny: PropTypes.func,
+    setReviewData: PropTypes.func,
+    reviewData: PropTypes.array
 }
 
 export default AdminNotify;
